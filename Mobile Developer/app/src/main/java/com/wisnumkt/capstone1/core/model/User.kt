df994b6f914233ab674133c87a7226de338e9948
BangkitVinaisCC <153369282+BangkitVinaisCC@users.noreply.github.com>
2023-12-22 17:19:20 +0700

    Delete Mobile Developer directory

--- Mobile Developer/app/src/main/java/com/wisnumkt/capstone1/core/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.wisnumkt.capstone1.core.model
-
-data class User(
-    val name: String = "",
-    val email: String = "",
-    val uid: String = ""
-    // Add other user-related fields as needed
-)
--- Mobile Developer/app/src/main/java/com/wisnumkt/capstone1/navigation/BottomBarItem.kt
+++
@@ -1,8 +0,0 @@
-package com.wisnumkt.capstone1.navigation
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class BottomBarItem(
-    val title: String,
-    val icon: ImageVector,
-    val screen: BottomNavItem)
